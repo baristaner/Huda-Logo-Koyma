d5b9e36917ce03fe1aaa0751288da00137969159
Barış Taner <legendbaris2003@gmail.com>
2019-12-30 23:16:13 +0300

    Update logokoy.lua

--- logokoy.lua
+++ logokoy.lua
@@ -22,18 +22,3 @@ end)
 
 
 
---[[
-CONFIG BILGILERI LUA HAKKINDA HİÇ BİR BİLGİSİ OLMAYANLAR OKUSUN
---------------------------------------------------------------------------------------------
-Resimin Sağ üst tarafta gözükmesi için önerilen ayarlar
-y = ScrH()/3 -250
-x = 15 +1020
-resimin boyutuna göre pozisyona değişmektedir kendinize göre arttırarak azaltarak deneyiniz.
---------------------------------------------------------------------------------------------
-Resimin Sol üst tarafta gözükmesi için önerilen ayarlar
-y = y = ScrH()/3 -250
-x = 15 +0
---------------------------------------------------------------------------------------------
-*dosyayı lua/autorun/client'e koyun
---------------------------------------------------------------------------------------------
---]]
