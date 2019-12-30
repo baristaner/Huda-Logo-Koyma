4388665c9df9bc4d3672c80892ca17495bf8bba1
Barış Taner <legendbaris2003@gmail.com>
2019-12-30 23:10:37 +0300

    Update logokoy.lua

--- logokoy.lua
+++ logokoy.lua
@@ -1,15 +1,6 @@
 --[[
 CONFIG BILGILERI LUA HAKKINDA HİÇ BİR BİLGİSİ OLMAYANLAR OKUSUN
---------------------------------------------------------------------------------------------
-Resimin Sağ üst tarafta gözükmesi için önerilen ayarlar
-config.boyuna = ScrH()/3 -250
-config.enine = 15 +1020
-resimin boyutuna göre pozisyona değişmektedir kendinize göre arttırarak azaltarak deneyiniz.
---------------------------------------------------------------------------------------------
-Resimin Sol üst tarafta gözükmesi için önerilen ayarlar
-config.boyuna = y = ScrH()/3 -250
-config.enine = 15 +0
---------------------------------------------------------------------------------------------
+------------------------------
 *dosyayı lua/autorun/client'e koyun
 --------------------------------------------------------------------------------------------
 --]]
@@ -17,10 +8,10 @@ config.enine = 15 +0
 config = {}
 
 config.logo = Material("gm_construct/flatsign");
-config.boyuna = ScrH()/3 -250 -- Boyuna 
-config.enine = 15 +1040 -- Enine
-config.resimh = 200
-config.resimw = 200
+config.boyuna = ScrH() * 0.0+150 -- Boyuna 
+config.enine = ScrW() * 0.0+150 -- Enine
+config.resimh = ScrH() * 0.0+150 
+config.resimw = ScrW() * 0.0+150 
 
 --ANLAMIYORSANIZ ELLEMEYIN
 hook.Add( "HUDPaint", "logohudkardes", function()
